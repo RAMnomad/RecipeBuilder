2db2629ce77c31d1d9d9afed2a47d26493077f54
Shawna Climer <shawnaclimer@gmail.com>
2015-06-15 09:41:10 -0500

    started recipe object

--- src/Recipe.java
+++ src/Recipe.java
@@ -2,7 +2,40 @@
 public class Recipe {
 	//TODO:save title, ingredients and instructions as recipe object.  
 	//functions to edit, delete and save to file
+	private String title;
+	private String ingredients; //change to an ArrayList?
+	private String instructions;//change to an ArrayList?
 	
+	public Recipe(){
+		title="New Recipe";
+		ingredients="Ingredients here";
+		instructions="Instructions here";
+		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(String ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public String getInstructions() {
+		return instructions;
+	}
+
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}	
 	
 
 }
