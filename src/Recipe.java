bfe4440a6522dea607a4dec351db5d020546a68f
Rachele M <ladyofnomads@hotmail.com>
2015-06-24 18:42:47 -0500

    Added json libs.

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
+
 
 public class Recipe {
 	//TODO:save title, ingredients and instructions as recipe object.  
 	//functions to edit, delete and save to file
 	private String title;
-	private String ingredients; //change to an ArrayList?
-	private String instructions;//change to an ArrayList?
+	private ArrayList<String> ingredients; //change to an ArrayList?
+	private ArrayList<String> instructions;//change to an ArrayList?
 	
 	public Recipe(){
 		title="New Recipe";
-		ingredients="Ingredients here";
-		instructions="Instructions here";
+		ingredients=null;
+		instructions=null;
 		
+	}
+
+	public Recipe(String title, ArrayList<String> ingredients, ArrayList<String> instructions) {
+		super();
+		this.title = title;
+		this.ingredients = ingredients;
+		this.instructions = instructions;
 	}
 
 	public String getTitle() {
@@ -21,19 +30,19 @@ public class Recipe {
 		this.title = title;
 	}
 
-	public String getIngredients() {
+	public ArrayList<String> getIngredients() {
 		return ingredients;
 	}
 
-	public void setIngredients(String ingredients) {
+	public void setIngredients(ArrayList<String> ingredients) {
 		this.ingredients = ingredients;
 	}
 
-	public String getInstructions() {
+	public ArrayList<String> getInstructions() {
 		return instructions;
 	}
 
-	public void setInstructions(String instructions) {
+	public void setInstructions(ArrayList<String> instructions) {
 		this.instructions = instructions;
 	}	
 	
